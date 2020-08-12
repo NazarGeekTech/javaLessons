1cdf9c5c6b5ae123aef1a60f8c41e3bb194f20c9
gethiskg <atai2497@gmail.com>
2020-08-12 20:17:43 +0600

    secondBunch

--- homeWorkLesson3/src/com/company/Main.java
+++ homeWorkLesson3/src/com/company/Main.java
@@ -6,25 +6,44 @@ import java.util.Arrays;
 
 public class Main {
     //    declared global array that can access from everywhere;
-    static double[] has15Elements = {12.34, 34.34, -34.4, 23.32, 89.90, -56.65, 95.89, -1233.45, 577.45, 2346.34, 387.23, 52.43, 3234.23, 245.64, 236.78};
+
 
     public static void main(String[] args) {
         // write your code here
-        double positiveCount = 0;
-        for (double i : has15Elements) {
-            if (i < 0) {
-                positiveCount++;
+        double[] has15Elements = {12.34, 34.34, -34.4, 23.32, 89.90, -56.65, 95.89, -1233.45, 577.45, 2346.34, 387.23, 52.43, 3234.23, 245.64, 236.78};
+        boolean facedNegative = false;
+
+        double summeryOfPositive = 0;
+        int count = 0;
+        double avarageSum = 0;
+
+        for (double a : has15Elements) {
+            if (a < 0) {
+                facedNegative = true;
                 continue;
+            } else if (facedNegative && a > 0) {
+                summeryOfPositive += a;
+                count++;
             }
-            System.out.println(positiveCount += i);
+            avarageSum = summeryOfPositive / count;
         }
-        System.out.println("Here are average numbers of positive down below ");
-        System.out.println(positiveCount / 12);
+        System.out.println("here is the summery " + summeryOfPositive);
+        System.out.println("here is a count  positive number " + count);
+        System.out.println("here is a average number of out array " + avarageSum);
 
-        // additional homeWork
-        Arrays.sort(has15Elements);
-        System.out.println(Arrays.toString(has15Elements));
+
+//        additional home Work
+        for (int i = 0; i < has15Elements.length; i++) {
+            double onTheTruck = has15Elements[i];
+            int a = i - 1;
+            while (a >= 0 && onTheTruck < has15Elements[a]) {
+                has15Elements[a + 1] = has15Elements[a];
+                a--;
+            }
+            has15Elements[a + 1] = onTheTruck;
+            System.out.println(Arrays.toString(has15Elements));
+        }
+
+
     }
-
-
 }
