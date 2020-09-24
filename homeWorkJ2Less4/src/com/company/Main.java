d472871a76097c5f3cd87123a522e0577a7b762e
gethiskg <atai2497@gmail.com>
2020-09-24 15:33:41 +0600

    secondTry

--- homeWorkJ2Less4/src/com/company/Main.java
+++ homeWorkJ2Less4/src/com/company/Main.java
@@ -1,50 +1,22 @@
 package com.company;
 
-import javax.swing.*;
-import java.util.function.DoubleToIntFunction;
-
 public class Main {
 
     public static void main(String[] args) {
-//        this side to count all the way ,
-        for (int i = 2; i <= 5; i++) {
-            Runner runner = new Runner("runner ");
+
+        for (int i = 1; i <=5 ; i++) {
+            Runner runner = new Runner("Runner",i);
             Thread thread = new Thread(runner);
-//this minus i just to get the correct number of the runner or else it would be the on higher due to the sout which i'm increasing with ++i ;
-            System.out.println(runner.getName() + --i + " gets a stick ");
             thread.start();
+
             try {
                 thread.join();
                 thread.sleep(5000);
             } catch (InterruptedException e) {
                 e.printStackTrace();
             }
-            System.out.println(runner.getName() + i + " runs to " + runner.getName() + ++i);
-            if (i == 5) {
-                System.out.println(runner.getName() + i + " gets a stick  ");
-                System.out.println(runner.getName() + i + " runs to finish ");
-            }
+        }
 
-        }
-// this thread for counting down starts
-        for (int i = 4; i >= 1; i--) {
-            Runner runner = new Runner("runner ");
-            Thread thread = new Thread(runner);
-            if(i == 4){
-                System.out.println(runner.getName() + ++i + " runs to " + runner.getName() + --i);
-            }else {
-                System.out.println(runner.getName() + ++i + " gets a stick ");
-                thread.start();
-                try {
-                    thread.join();
-                    thread.sleep(5000);
-                } catch (InterruptedException e) {
-                    e.printStackTrace();
-                }
-                System.out.println(runner.getName() + i + " runs to " + runner.getName() + --i);
-            }
-        }
-        System.out.println("Runner 1 gets a stick");
     }
 }
 
--- homeWorkJ2Less4/src/com/company/Runner.java
+++ homeWorkJ2Less4/src/com/company/Runner.java
@@ -6,10 +6,14 @@ package com.company;
 
 public class Runner implements Runnable{
     private String name;
+    private  int number;
 
 
 
-
+    public Runner(String name,int number){
+        this.name = name;
+        this.number  = number;
+    }
     public Runner(String name) {
         this.name = name;
     }
@@ -20,8 +24,48 @@ public String getName (){
     @Override
     public void run() {
         synchronized (this) {
+                if (number == 1) {
+
+                    for (int i = 1; i <= 5; i++) {
+                        try {
+                            Thread.sleep(5000);
+                        } catch (InterruptedException e) {
+                            e.printStackTrace();
+                        }
+                            System.out.println(this.getName() + " " + this.number + " gets a stick ");
+                            System.out.println(this.getName() + " " + this.number + " runs to " + this.getName() + " " + ++this.number);
+                            if (i == 4) {
+                                System.out.println(this.getName() + " " + this.number + " gets a stick ");
+                                System.out.println(this.getName() + " " + this.number + " runs to finish");
+                                break;
+                            }
+                        }
+
+
+                }
+                if (number == 5) {
+
+                    for (int i = 5; i >= 1; i--) {
+                        try {
+                            Thread.sleep(5000);
+                        } catch (InterruptedException e) {
+                            e.printStackTrace();
+                        }
+                        System.out.println(this.getName() + " " + this.number + " runs to " + this.getName() + " " + --this.number);
+                        System.out.println(this.getName() + " " + this.number + " gets a stick ");
+                        if (i == 2) {
+                            break;
+                        }
+                    }
+                    try {
+                        Thread.sleep(5000);
+                    } catch (InterruptedException e) {
+                        e.printStackTrace();
+                    }
+                }
+
 
         }
-
     }
 }
+
